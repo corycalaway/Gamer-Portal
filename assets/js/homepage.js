757c88df32ef445662014feb2a684c27db45981d
corycalaway <cory.c.calaway@gmail.com>
2020-12-19 14:03:51 -0700

    api pulls name then sends name into second api

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,41 +1,43 @@
-var apiDataFunction = function() {
+//var gameName = "";
 
-
-    var apiUrl =  "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15"
+var apiDataFunction = function () {
+  //var apiUrl =  "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15"
+  var apiUrl = "https://api.rawg.io/api/games?dates=2020-01-01,2020-12-31&ordering=-added?key=f58ccbebc042468c979cde0ac7353b65"
     
+    fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+      var gameName = data.results[0].name
+      console.log(gameName)
+      apiDataFunctionTwo(gameName);
+    });
+  });
+};
 
-    
-    fetch(apiUrl).then(function(response){
-    response.json().then(function(data){
-        console.log(data)
-    })})
-    
-    };
+var apiDataFunctionTwo = function (gameName) {
+  // var apiUrlTwo =  "https://api.rawg.io/api/platforms?key=f58ccbebc042468c979cde0ac7353b65"
+  // fetches data for anticipated 2020 games
+  var apiUrlTwo = "https://www.cheapshark.com/api/1.0/games?title=" + gameName + "&limit=60&exact=0";
+  //var apiUrlTwo = 'https://api.rawg.io/api/games?id=3328?'
+  //var apiUrlTwo = 'https://api.rawg.io/api/genres'
+  // var apiUrlTwo = 'https://api.rawg.io/api/platforms'
+  //var apiUrlTwo =  'https://api.rawg.io/api/games?dates=2020-01-01,2020-12-31&ordering=-added?key=f58ccbebc042468c979cde0ac7353b65'
+  // var apiUrlTwo =  'https://api.rawg.io/api/games?dates=2020-01-01,2020-12-31&platforms=4&key=f58ccbebc042468c979cde0ac7353b65'
+  //f58ccbebc042468c979cde0ac7353b65
 
-    var apiDataFunctionTwo = function() {
+  fetch(apiUrlTwo).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
+};
+
+apiDataFunction();
 
 
-       // var apiUrlTwo =  "https://api.rawg.io/api/platforms?key=f58ccbebc042468c979cde0ac7353b65"
-       // fetches data for anticipated 2020 games
-       var apiUrlTwo =  'https://api.rawg.io/api/games?dates=2020-01-01,2020-12-31&ordering=-added?key=f58ccbebc042468c979cde0ac7353b65'
-        
-        //f58ccbebc042468c979cde0ac7353b65
+// modal for searching game types
+$(document).ready(function () {
+  $(".modal").modal();
+});
 
-    
-        
-        fetch(apiUrlTwo).then(function(response){
-        response.json().then(function(data){
-            console.log(data)
-        })})
-        
-        };
-    
-    //apiDataFunction();
-    apiDataFunctionTwo();
-
-    // modal for searching game types
-    $(document).ready(function(){
-        $('.modal').modal();
-      });
-
-      //https://corycalaway.github.io/gamer-portal/
+//https://corycalaway.github.io/gamer-portal/
