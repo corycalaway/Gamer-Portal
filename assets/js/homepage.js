0205489368969bf8c800188000c72677534a4a5b
Bret Short <bretshort@Ellens-MacBook-Pro.local>
2020-12-19 15:39:02 -0700

    adding changes

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,9 @@
 //var gameName = "";
 
+//var action = document.getElementById('action').src='https://media.rawg.io/media/games/b45/b45575f34285f2c4479c9a5f719d972e.jpg';
+
+$("#action").css("background-image", "url('https://media.rawg.io/media/games/b45/b45575f34285f2c4479c9a5f719d972e.jpg')");
+
 var apiDataFunction = function () {
   //var apiUrl =  "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15"
   var apiUrl = "https://api.rawg.io/api/games?dates=2020-01-01,2020-12-31&ordering=-added?key=f58ccbebc042468c979cde0ac7353b65"
