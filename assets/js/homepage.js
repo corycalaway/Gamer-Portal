e3bde4140377b89ddc36dbc9d12a83ca4bf170bb
Bret Short <bretshort@Ellens-MacBook-Pro.local>
2020-12-22 20:44:21 -0700

    refactoring code

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -18,161 +18,209 @@ var apiDataFunction = function () {
       console.log(data);
 
       gameGenre = 0;
+      var modalSelect = document.getElementById("modal1");
+      var instance = M.Modal.getInstance(modalSelect);
+      var modalFormat = document.getElementById('modalFormat')
 
+      for (let i = 0; i < 19; i++) {
+       // const element = array[i];
+       console.log(data.results[i])
+      console.log(i)
+      var modalDiv = document.createElement("div");
+      
+      modalDiv.classList.add('modalImg', 'col', 'l3', 'genreClass' + i)
+      modalFormat.appendChild(modalDiv);
+
+        var genreText = (data.results[i].name)
+        console.log(genreText)
+
+          
+      $(".genreClass" + i)
+        //.css("background-image", "url(" + data.results[i].image_background + ")")
+        .css("background-image", `url(${data.results[i].image_background})`).text(genreText)
+        .click(function () {
+               gameGenre = data.results[i].id;
+              instance.close();
+             apiDataFunctionTwo(gameGenre);
+             });
+        console.log(data.results[i].id)
+        
+      }
       // modal images for generes
-      var actionImg = data.results[0].image_background;
-      var indieImg = data.results[1].image_background;
-      var adventureImg = data.results[2].image_background;
-      var rpgImg = data.results[3].image_background;
-      var strategyImg = data.results[4].image_background;
-      var shooterImg = data.results[5].image_background;
-      var casualImg = data.results[6].image_background;
-      var simulationImg = data.results[7].image_background;
-      var puzzleImg = data.results[8].image_background;
-      var arcadeImg = data.results[9].image_background;
-      var platformerImg = data.results[10].image_background;
-      var racingImg = data.results[11].image_background;
-      var SportsImg = data.results[12].image_background;
-      var mmoImg = data.results[13].image_background;
-      var fightingImg = data.results[14].image_background;
-      var familyImg = data.results[15].image_background;
-      var boardgamesImg = data.results[16].image_background;
-      var educationalImg = data.results[17].image_background;
-      var cardImg = data.results[18].image_background;
+      // var actionImg = data.results[0].image_background;
+      // var indieImg = data.results[1].image_background;
+      // var adventureImg = data.results[2].image_background;
+      // var rpgImg = data.results[3].image_background;
+      // var strategyImg = data.results[4].image_background;
+      // var shooterImg = data.results[5].image_background;
+      // var casualImg = data.results[6].image_background;
+      // var simulationImg = data.results[7].image_background;
+      // var puzzleImg = data.results[8].image_background;
+      // var arcadeImg = data.results[9].image_background;
+      // var platformerImg = data.results[10].image_background;
+      // var racingImg = data.results[11].image_background;
+      // var SportsImg = data.results[12].image_background;
+      // var mmoImg = data.results[13].image_background;
+      // var fightingImg = data.results[14].image_background;
+      // var familyImg = data.results[15].image_background;
+      // var boardgamesImg = data.results[16].image_background;
+      // var educationalImg = data.results[17].image_background;
+      // var cardImg = data.results[18].image_background;
+      // var modalSelect = document.getElementById("modal1");
+      // var instance = M.Modal.getInstance(modalSelect);
 
       //images and click events
-      $("#action")
-        .css("background-image", "url(" + actionImg + ")")
-        .click(function () {
-          gameGenre = data.results[0].id;
-          apiDataFunctionTwo(gameGenre);
-        });
+      // $("#action")
+      //   .css("background-image", "url(" + actionImg + ")")
+      //   .click(function () {
+      //     gameGenre = data.results[0].id;
+      //     instance.close();
+      //     apiDataFunctionTwo(gameGenre);
+      //   });
 
-      $("#indie")
-        .css("background-image", "url(" + indieImg + ")")
-        .click(function () {
-          gameGenre = data.results[1].id;
-          apiDataFunctionTwo(gameGenre);
-        });
+      // $("#indie")
+      //   .css("background-image", "url(" + indieImg + ")")
+      //   .click(function () {
+      //     gameGenre = data.results[1].id;
+      //     instance.close();
+      //     apiDataFunctionTwo(gameGenre);
+      //   });
 
-      $("#adventure")
-        .css("background-image", "url(" + adventureImg + ")")
-        .click(function () {
-          gameGenre = data.results[2].id;
-          apiDataFunctionTwo(gameGenre);
-        });
+      // $("#adventure")
+      //   .css("background-image", "url(" + adventureImg + ")")
+      //   .click(function () {
+      //     gameGenre = data.results[2].id;
+      //     instance.close();
+      //     apiDataFunctionTwo(gameGenre);
+      //   });
 
-      $("#rpg")
-        .css("background-image", "url(" + rpgImg + ")")
-        .click(function () {
-          gameGenre = data.results[3].id;
-          apiDataFunctionTwo(gameGenre);
-        });
+      // $("#rpg")
+      //   .css("background-image", "url(" + rpgImg + ")")
+      //   .click(function () {
+      //     gameGenre = data.results[3].id;
+      //     instance.close();
+      //     apiDataFunctionTwo(gameGenre);
+      //   });
 
-      $("#strategy")
-        .css("background-image", "url(" + strategyImg + ")")
-        .click(function () {
-          gameGenre = data.results[4].id;
-          apiDataFunctionTwo(gameGenre);
-        });
+      // $("#strategy")
+      //   .css("background-image", "url(" + strategyImg + ")")
+      //   .click(function () {
+      //     gameGenre = data.results[4].id;
+      //     instance.close();
+      //     apiDataFunctionTwo(gameGenre);
+      //   });
 
-      $("#shooter")
-        .css("background-image", "url(" + shooterImg + ")")
-        .click(function () {
-          gameGenre = data.results[5].id;
-          apiDataFunctionTwo(gameGenre);
-        });
+      // $("#shooter")
+      //   .css("background-image", "url(" + shooterImg + ")")
+      //   .click(function () {
+      //     gameGenre = data.results[5].id;
+      //     instance.close();
+      //     apiDataFunctionTwo(gameGenre);
+      //   });
 
-      $("#casual")
-        .css("background-image", "url(" + casualImg + ")")
-        .click(function () {
-          gameGenre = data.results[6].id;
-          apiDataFunctionTwo(gameGenre);
-        });
+      // $("#casual")
+      //   .css("background-image", "url(" + casualImg + ")")
+      //   .click(function () {
+      //     gameGenre = data.results[6].id;
+      //     instance.close();
+      //     apiDataFunctionTwo(gameGenre);
+      //   });
 
-      $("#simulation")
-        .css("background-image", "url(" + simulationImg + ")")
-        .click(function () {
-          gameGenre = data.results[7].id;
-          apiDataFunctionTwo(gameGenre);
-        });
+      // $("#simulation")
+      //   .css("background-image", "url(" + simulationImg + ")")
+      //   .click(function () {
+      //     gameGenre = data.results[7].id;
+      //     instance.close();
+      //     apiDataFunctionTwo(gameGenre);
+      //   });
 
-      $("#puzzle")
-        .css("background-image", "url(" + puzzleImg + ")")
-        .click(function () {
-          gameGenre = data.results[8].id;
-          apiDataFunctionTwo(gameGenre);
-        });
+      // $("#puzzle")
+      //   .css("background-image", "url(" + puzzleImg + ")")
+      //   .click(function () {
+      //     gameGenre = data.results[8].id;
+      //     instance.close();
+      //     apiDataFunctionTwo(gameGenre);
+      //   });
 
-      $("#arcade")
-        .css("background-image", "url(" + arcadeImg + ")")
-        .click(function () {
-          gameGenre = data.results[9].id;
-          apiDataFunctionTwo(gameGenre);
-        });
+      // $("#arcade")
+      //   .css("background-image", "url(" + arcadeImg + ")")
+      //   .click(function () {
+      //     gameGenre = data.results[9].id;
+      //     instance.close();
+      //     apiDataFunctionTwo(gameGenre);
+      //   });
 
-      $("#platformer")
-        .css("background-image", "url(" + platformerImg + ")")
-        .click(function () {
-          gameGenre = data.results[10].id;
-          apiDataFunctionTwo(gameGenre);
-        });
+      // $("#platformer")
+      //   .css("background-image", "url(" + platformerImg + ")")
+      //   .click(function () {
+      //     gameGenre = data.results[10].id;
+      //     instance.close();
+      //     apiDataFunctionTwo(gameGenre);
+      //   });
 
-      $("#racing")
-        .css("background-image", "url(" + racingImg + ")")
-        .click(function () {
-          gameGenre = data.results[11].id;
-          apiDataFunctionTwo(gameGenre);
-        });
+      // $("#racing")
+      //   .css("background-image", "url(" + racingImg + ")")
+      //   .click(function () {
+      //     gameGenre = data.results[11].id;
+      //     instance.close();
+      //     apiDataFunctionTwo(gameGenre);
+      //   });
 
-      $("#sports")
-        .css("background-image", "url(" + SportsImg + ")")
-        .click(function () {
-          gameGenre = data.results[12].id;
-          apiDataFunctionTwo(gameGenre);
-        });
+      // $("#sports")
+      //   .css("background-image", "url(" + SportsImg + ")")
+      //   .click(function () {
+      //     gameGenre = data.results[12].id;
+      //     instance.close();
+      //     apiDataFunctionTwo(gameGenre);
+      //   });
 
-      $("#mmo")
-        .css("background-image", "url(" + mmoImg + ")")
-        .click(function () {
-          gameGenre = data.results[13].id;
-          apiDataFunctionTwo(gameGenre);
-        });
+      // $("#mmo")
+      //   .css("background-image", "url(" + mmoImg + ")")
+      //   .click(function () {
+      //     gameGenre = data.results[13].id;
+      //     instance.close();
+      //     apiDataFunctionTwo(gameGenre);
+      //   });
 
-      $("#fighting")
-        .css("background-image", "url(" + fightingImg + ")")
-        .click(function () {
-          gameGenre = data.results[14].id;
-          apiDataFunctionTwo(gameGenre);
-        });
+      // $("#fighting")
+      //   .css("background-image", "url(" + fightingImg + ")")
+      //   .click(function () {
+      //     gameGenre = data.results[14].id;
+      //     instance.close();
+      //     apiDataFunctionTwo(gameGenre);
+      //   });
 
-      $("#family")
-        .css("background-image", "url(" + familyImg + ")")
-        .click(function () {
-          gameGenre = data.results[15].id;
-          apiDataFunctionTwo(gameGenre);
-        });
+      // $("#family")
+      //   .css("background-image", "url(" + familyImg + ")")
+      //   .click(function () {
+      //     gameGenre = data.results[15].id;
+      //     instance.close();
+      //     apiDataFunctionTwo(gameGenre);
+      //   });
 
-      $("#boardgames")
-        .css("background-image", "url(" + boardgamesImg + ")")
-        .click(function () {
-          gameGenre = data.results[16].id;
-          apiDataFunctionTwo(gameGenre);
-        });
+      // $("#boardgames")
+      //   .css("background-image", "url(" + boardgamesImg + ")")
+      //   .click(function () {
+      //     gameGenre = data.results[16].id;
+      //     instance.close();
+      //     apiDataFunctionTwo(gameGenre);
+      //   });
 
-      $("#educational")
-        .css("background-image", "url(" + educationalImg + ")")
-        .click(function () {
-          gameGenre = data.results[17].id;
-          apiDataFunctionTwo(gameGenre);
-        });
+      // $("#educational")
+      //   .css("background-image", "url(" + educationalImg + ")")
+      //   .click(function () {
+      //     gameGenre = data.results[17].id;
+      //     instance.close();
+      //     apiDataFunctionTwo(gameGenre);
+      //   });
 
-      $("#card")
-        .css("background-image", "url(" + cardImg + ")")
-        .click(function () {
-          gameGenre = data.results[18].id;
-          apiDataFunctionTwo(gameGenre);
-        });
+      // $("#card")
+      //   .css("background-image", "url(" + cardImg + ")")
+      //   .click(function () {
+      //     gameGenre = data.results[18].id;
+      //     instance.close();
+      //     apiDataFunctionTwo(gameGenre);
+      //   });
 
       console.log(gameGenre);
     });
@@ -289,8 +337,9 @@ var resultVideo = function(resultVideoData) {
  returnedVideo.src = resultVideoData
  console.log(returnedVideo)
  var gameVideo = document.getElementById("gameVideo")
+ gameVideo.autoplay = true;
   gameVideo.load()
-}
+};
 
 
 apiDataFunction();
