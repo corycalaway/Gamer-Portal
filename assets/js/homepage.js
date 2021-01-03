626bfdbe68e3c7d82f18219d095ac8cc4909189d
corycalaway <cory.c.calaway@gmail.com>
2021-01-03 00:11:05 -0700

    restored data lost from broken branch

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -5,6 +5,10 @@
 //$("#action").css("background-image", "url('https://media.rawg.io/media/games/b45/b45575f34285f2c4479c9a5f719d972e.jpg')");
 //$("#action").css("background-image", "url('https://media.rawg.io/media/games/b45/b45575f34285f2c4479c9a5f719d972e.jpg')");
 
+// date time function
+
+var dateTime = luxon.DateTime.local();
+
 var apiDataFunction = function () {
   //var apiUrl =  "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15"
   //var apiUrl = "https://api.rawg.io/api/games?dates=2020-01-01,2020-12-31&ordering=-added?key=f58ccbebc042468c979cde0ac7353b65"
@@ -62,7 +66,7 @@ var apiDataFunctionTwo = function (gameGenre) {
     "https://api.rawg.io/api/games?genres=" +
     gameGenre +
     // most popular games from 2000 to 2020
-    "&dates=2000-01-01,2020-12-31&ordering=-added";
+    "&dates=2000-01-01,2020-12-31&ordering=-added&platforms=4";
   //var apiUrlTwo = "https://www.cheapshark.com/api/1.0/games?title=" + gameName + "&limit=60&exact=0";
   //var apiUrlTwo = 'https://api.rawg.io/api/games?id=3328?'
   //var apiUrlTwo = 'https://api.rawg.io/api/genres'
@@ -197,6 +201,120 @@ $(document).ready(function () {
   $(".modal").modal();
 });
 
+
+// gets data for highest games
+var highestRatedGames = function() { 
+
+console.log(dateTime)
+
+// generates random number to show games from different generes
+var randomNumGen = Math.floor(Math.random() * 15);
+
+var apiHighestRated = 'https://api.rawg.io/api/games?dates=2001-01-01,' + dateTime + '&ordering=-rating'
+
+  // generates random number to change randomly selected game after each click.
+  
+  fetch(apiHighestRated).then(function (response) {
+    response.json().then(function (data) {
+    console.log(data)
+    console.log(randomNumGen)
+   console.log(data.results[randomNumGen].name)
+   console.log(data.results[randomNumGen].rating)
+   console.log(data.results[randomNumGen + 1].name)
+   console.log(data.results[randomNumGen + 2].name)
+   console.log(data.results[randomNumGen + 3].name)
+   console.log(data.results[randomNumGen + 4].name)
+      
+      // selects top game from 5 generes
+   // creates loop for highest rated data
+   // need to generate alternate image for when background image = null
+   for (let i = 0; i < 5; i++) {
+
+    var highestRatedCard = $('<div>')
+    .addClass('cardDisplay card deep-orange')
+
+    var highestRatedGameName = $('<div>')
+    .addClass('highestRatedFormatText')
+    .text(data.results[randomNumGen + i].name)
+
+    var highestRatedGameBox = $('<div>')
+    .addClass('imageSize')
+
+    if (data.results[randomNumGen + i].background_image === null) {
+      console.log('fail')
+      var highestRatedGameOne = $('<i>')
+      .addClass('material-icons iconImageFail')
+      .attr('id', 'highestRatedGame' + i)
+      .text('stars')
+
+    } else {
+   var highestRatedGameOne = $('<div>')
+   .addClass('highestRatedFormat')
+   .attr('id', 'highestRatedGame' + i)
+   .css("background-image", "url(" + data.results[randomNumGen + i].background_image + ")")
+    }
+   $("#highestRatedGames").append(highestRatedCard)
+   
+   $(highestRatedCard).append(highestRatedGameBox)
+   $(highestRatedCard).append(highestRatedGameName)
+   $(highestRatedGameBox).append(highestRatedGameOne)
+   
+   }
+
+  //  var highestRatedGameTwo = $('<div>')
+  //  .addClass('highestRatedFormat')
+  //  .attr('id', 'highestRatedGameTwo')
+  //  .css("background-image", "url(" + data.results[randomNumGen + 1].background_image + ")")
+
+  //  var highestRatedGameThree = $('<div>')
+  //  .addClass('highestRatedFormat')
+  //  .attr('id', 'highestRatedGameThree')
+  //  .css("background-image", "url(" + data.results[randomNumGen + 2].background_image + ")")
+
+  //  var highestRatedGameFour = $('<div>')
+  //  .addClass('highestRatedFormat')
+  //  .attr('id', 'highestRatedGameFour')
+  //  .css("background-image", "url(" + data.results[randomNumGen + 3].background_image + ")")
+
+  //  var highestRatedGameFive = $('<div>')
+  //  .addClass('highestRatedFormat')
+  //  .attr('id', 'highestRatedGameFive')
+  //  .css("background-image", "url(" + data.results[randomNumGen + 4].background_image + ")")
+
+  //  $("#highestRatedGames").append(highestRatedGameOne)
+  //  $("#highestRatedGames").append(highestRatedGameTwo)
+  //  $("#highestRatedGames").append(highestRatedGameThree)
+  //  $("#highestRatedGames").append(highestRatedGameFour)
+  //  $("#highestRatedGames").append(highestRatedGameFive)
+//    var highestRatedGames = document.querySelector("#highestRatedGames");
+
+// console.log(data.results[randomNumGen].background_image)
+//    //highestRatedGames.innerHTML = "<img src='" + data.results[randomNumGen].background_image + "' />"
+
+//    //.css("background-image", "url(" + data.results[i].image_background + ")")
+//    highestRatedGames.classList.add('highestRatedFormat')
+   
+//    $("#highestRatedGames").css("background-image", "url(" + data.results[randomNumGen].background_image + ")")
+     
+      
+    });
+  });
+};
+highestRatedGames();
+
+
+// var randomNumGen = Math.floor(Math.random() * 20);
+  
+//   fetch(apiUrlTwo).then(function (response) {
+//     response.json().then(function (data) {
+//    console.log(data)
+
+//       var gameName = data.results[randomNumGen].name;
+//       console.log(gameName)
+     
+//       // later look into adding background image when clip is null
+//       var resultVideoData = (data.results[randomNumGen].clip.clip)
+//       console.log(data.results[randomNumGen].metacritic)
 // var apiDataFunctionThree = function () {
 
 // var apiUrlTwo = "https://api.rawg.io/api/games?ordering=-metacritic"
