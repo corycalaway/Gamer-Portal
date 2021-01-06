1c04ea433ae1fdcd26755073d2bc0c9570d04a01
corycalaway <cory.c.calaway@gmail.com>
2021-01-06 15:19:39 -0700

    added no metacritic score found

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -12,7 +12,7 @@ var list = JSON.parse(localStorage.getItem("saveGameId")) || [];
 while (list.length > 6) {
   localStorage.removeItem(0)
   list.shift();
-  console.log(list)
+
 
   localStorage.setItem("saveGameId", JSON.stringify(list));
 }
@@ -28,7 +28,6 @@ var apiUrlHistory = 'https://api.rawg.io/api/games/' + list[i];
 
 fetch(apiUrlHistory).then(function(response) {
   response.json().then(function (data) {
-    console.log(data)
     recentlySearchedGames
 
     var recentlySearchedCard = $('<div>')
@@ -41,19 +40,12 @@ fetch(apiUrlHistory).then(function(response) {
     var recentlySearchedGameBox = $('<div>')
      .addClass('imageSize')
 
-//     if (data.results[randomNumGen + i].background_image === null) {
-//       console.log('fail')
-//       var highestRatedGameOne = $('<i>')
-//       .addClass('material-icons iconImageFail')
-//       .attr('id', 'highestRatedGame' + i)
-//       .text('stars')
 
-//     } else {
     var recentlySearchedGameOne = $('<div>')
     .addClass('recentlySearchedFormat')
    .attr('id', 'recentlySearchedGame')
    .css("background-image", "url(" + data.background_image + ")")
-//     }
+
    $("#recentlySearchedGames").append(recentlySearchedCard)
    
    $(recentlySearchedCard).append(recentlySearchedGameBox)
@@ -62,7 +54,6 @@ fetch(apiUrlHistory).then(function(response) {
 
 
     $('#clickElement' + list[i]).on('click',function(){
-      console.log(list[i])
 
       var gameId = list[i]
       gameIdFunction(gameId)
@@ -72,69 +63,9 @@ fetch(apiUrlHistory).then(function(response) {
 };
 apiSearchHistory()
 
-// var apiHighestRated = 'https://api.rawg.io/api/games?dates=2001-01-01,' + dateTime + '&ordering=-rating'
-
-//   // generates random number to change randomly selected game after each click.
-  
-//   fetch(apiHighestRated).then(function (response) {
-//     response.json().then(function (data) {
-//     console.log(data)
-//     console.log(randomNumGen)
-//    console.log(data.results[randomNumGen].name)
-//    console.log(data.results[randomNumGen].rating)
-//    console.log(data.results[randomNumGen + 1].name)
-//    console.log(data.results[randomNumGen + 2].name)
-//    console.log(data.results[randomNumGen + 3].name)
-//    console.log(data.results[randomNumGen + 4].name)
-      
-//       // selects top game from 5 generes
-//    // creates loop for highest rated data
-//    // need to generate alternate image for when background image = null
-//    for (let i = 0; i < 5; i++) {
-
-//     var highestRatedCard = $('<div>')
-//     .addClass('cardDisplay card red')
-
-//     var highestRatedGameName = $('<div>')
-//     .addClass('highestRatedFormatText')
-//     .text(data.results[randomNumGen + i].name)
-
-//     var highestRatedGameBox = $('<div>')
-//     .addClass('imageSize')
-
-//     if (data.results[randomNumGen + i].background_image === null) {
-//       console.log('fail')
-//       var highestRatedGameOne = $('<i>')
-//       .addClass('material-icons iconImageFail')
-//       .attr('id', 'highestRatedGame' + i)
-//       .text('stars')
-
-//     } else {
-//    var highestRatedGameOne = $('<div>')
-//    .addClass('highestRatedFormat')
-//    .attr('id', 'highestRatedGame' + i)
-//    .css("background-image", "url(" + data.results[randomNumGen + i].background_image + ")")
-//     }
-//    $("#highestRatedGames").append(highestRatedCard)
-   
-//    $(highestRatedCard).append(highestRatedGameBox)
-//    $(highestRatedCard).append(highestRatedGameName)
-//    $(highestRatedGameBox).append(highestRatedGameOne)
-   
-//    }
-
-
-
-
-
-
-
-
 
 var apiDataFunction = function () {
-  //var apiUrl =  "https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15"
-  //var apiUrl = "https://api.rawg.io/api/games?dates=2020-01-01,2020-12-31&ordering=-added?key=f58ccbebc042468c979cde0ac7353b65"
-  //var apiUrl = "https://api.rawg.io/api/games?genres=4"
+
   var apiUrl = "https://api.rawg.io/api/genres";
 
   //ask about fetching specific string values.
@@ -190,30 +121,19 @@ var apiDataFunctionTwo = function (gameGenre) {
     gameGenre +
     // most popular games from 2000 to 2020
     "&dates=2000-01-01,2020-12-31&ordering=-added&platforms=4";
-  //var apiUrlTwo = "https://www.cheapshark.com/api/1.0/games?title=" + gameName + "&limit=60&exact=0";
-  //var apiUrlTwo = 'https://api.rawg.io/api/games?id=3328?'
-  //var apiUrlTwo = 'https://api.rawg.io/api/genres'
-  // var apiUrlTwo = 'https://api.rawg.io/api/platforms'
-  //var apiUrlTwo =  'https://api.rawg.io/api/games?dates=2020-01-01,2020-12-31&ordering=-added?key=f58ccbebc042468c979cde0ac7353b65'
-  // var apiUrlTwo =  'https://api.rawg.io/api/games?dates=2020-01-01,2020-12-31&platforms=4&key=f58ccbebc042468c979cde0ac7353b65'
-  //f58ccbebc042468c979cde0ac7353b65
 
   // generates random number to change randomly selected game after each click.
   var randomNumGen = Math.floor(Math.random() * 20);
   
   fetch(apiUrlTwo).then(function (response) {
     response.json().then(function (data) {
-   console.log(data)
 
       var gameName = data.results[randomNumGen].name;
       var gameId = data.results[randomNumGen].id;
-      console.log(gameName)
-      console.log(gameId)
 
       //restores local saved data to last five searches
       while (list.length > 5) {
         list.shift();
-        console.log(list)
         localStorage.setItem("saveGameId", JSON.stringify(list));
       }
 
@@ -238,17 +158,10 @@ var gameIdFunction = function(gameId, gameName) {
 
       fetch(apiUrlId).then(function(response) {
         response.json().then(function (data) {
-          console.log('under')
-          console.log(data)
 
-          gameName=data.name
+          gameName = data.name
 
-
-
-
-         
-      
-          // checks if video is available and dynamically adds and removes element
+      // checks if video is available and dynamically adds and removes element
       // var gameVideo = document.getElementById("gameVideo")
       if (data.clip === null) {
         var resultVideoData = (data.background_image)
@@ -280,22 +193,46 @@ var gameIdFunction = function(gameId, gameName) {
 //                 </video> */}
 
 
-      console.log(data.metacritic)
 
       // metacritic score
-    
+    console.log(data)
        var metacriticText = (data.metacritic)
-       
+       var ratingText = (data.rating)
+  console.log(metacriticText)
+
+      // when no metacritic score is found
+       if (metacriticText === null) {
+        $(".metacriticStyle").addClass("bluemeta").text('No Metacritic Score Found')
+       } else {
+
        $(".metacriticStyle")
        .text(metacriticText)
+
        if (metacriticText >= 80) {
          $(".metacriticStyle").addClass("greenmeta")
+
        } else if (metacriticText >= 60 && metacriticText <= 79){
           $(".metacriticStyle").addClass("yellowmeta")
        } else {
-         $("metacriticStyle").addClass("redmeta")
+         $(".metacriticStyle").addClass("redmeta")
        } 
+      }
 
+       // user rating score
+       if (ratingText === null) {
+        $(".ratingStyle").addClass("bluemeta").text('No User Rating Found')
+
+       } else {
+       $(".ratingStyle")
+       .text(ratingText)
+       if (ratingText >= 4) {
+         $(".ratingStyle").addClass("greenmeta")
+       } else if (ratingText >= 3 && ratingText < 4){
+          $(".ratingStyle").addClass("yellowmeta")
+       } else {
+         $(".ratingStyle").addClass("redmeta")
+       } 
+      }
       
    
 
@@ -315,9 +252,6 @@ var apiDataFunctionCheapShark = function (gameName) {
 
   fetch(apiUrlCheapShark).then(function (response) {
     response.json().then(function (data) {
-      console.log(data)
-      //String.prototype.indexOf()
-    //  var gameId = data[0].steamAppID;
       
     // if no deal is found display text
     if (data.length === 0) {
@@ -339,26 +273,6 @@ var apiDataFunctionCheapShark = function (gameName) {
     }
 
 
-      //indexes position of text
-      var test2 = data[0].external;
-      console.log(test2);
-      var test3 = data[0].internalName;
-      console.log(test3);
-      console.log(gameName);
-      var testing = test2.indexOf(gameName);
-      console.log(testing);
-      var testFilter = data.filter(
-        (gameDetails) => gameDetails.external.indexOf(gameName) >= 0
-      );
-      console.log(testFilter);
-      var testFilter = data.filter(
-        (gameDetails) => gameDetails.external.indexOf(gameName) === 0
-      );
-      
-      
-      //console.log(testFilter);
-
-
     });
   });
 };
@@ -373,7 +287,6 @@ var resultVideo = function(resultVideoData) {
  var returnedVideo = document.getElementById("returnedVideo");
  //document.getElementById('#returnedVideo').src="images/my_other_image.png"
  returnedVideo.src = resultVideoData
- console.log(returnedVideo)
  
  gameVideo.autoplay = true;
   gameVideo.load()
@@ -391,8 +304,6 @@ $(document).ready(function () {
 // gets data for highest games
 var highestRatedGames = function() { 
 
-console.log(dateTime)
-
 // generates random number to show games from different generes
 var randomNumGen = Math.floor(Math.random() * 15);
 
@@ -402,15 +313,7 @@ var apiHighestRated = 'https://api.rawg.io/api/games?dates=2001-01-01,' + dateTi
   
   fetch(apiHighestRated).then(function (response) {
     response.json().then(function (data) {
-    console.log(data)
-    console.log(randomNumGen)
-   console.log(data.results[randomNumGen].name)
-   console.log(data.results[randomNumGen].rating)
-   console.log(data.results[randomNumGen + 1].name)
-   console.log(data.results[randomNumGen + 2].name)
-   console.log(data.results[randomNumGen + 3].name)
-   console.log(data.results[randomNumGen + 4].name)
-      
+
       // selects top game from 5 generes
    // creates loop for highest rated data
    // need to generate alternate image for when background image = null
@@ -428,7 +331,6 @@ var apiHighestRated = 'https://api.rawg.io/api/games?dates=2001-01-01,' + dateTi
     .addClass('imageSize')
 
     if (data.results[randomNumGen + i].background_image === null) {
-      console.log('fail')
       var highestRatedGameOne = $('<i>')
       .addClass('material-icons iconImageFail')
       .attr('id', 'highestRatedGame' + i)
@@ -450,7 +352,6 @@ var apiHighestRated = 'https://api.rawg.io/api/games?dates=2001-01-01,' + dateTi
    // click card for highest rated section to search for game
 
    $('#highestRatedCard' + i).on('click',function(){
-  console.log('pass')
 
     var gameId = data.results[randomNumGen + i].id
     gameIdFunction(gameId)
@@ -472,8 +373,6 @@ $(document).ready(function(){
 
 // most ancticipated games
 var anticipatedGames = function() { 
-
-  console.log(dateTime)
   
   // generates random number to show games from different generes
   var randomNumGen = Math.floor(Math.random() * 15);
@@ -484,7 +383,6 @@ var anticipatedGames = function() {
     
     fetch(apiAnticipated).then(function (response) {
       response.json().then(function (data) {
-      console.log(data)
         
         // selects top game from 5 generes
      // creates loop for highest rated data
@@ -503,7 +401,6 @@ var anticipatedGames = function() {
       .addClass('imageSize')
   
       if (data.results[randomNumGen + i].background_image === null) {
-        console.log('fail')
         var anticipatedGameOne = $('<i>')
         .addClass('material-icons iconImageFail')
         .attr('id', 'anticipatedGame' + i)
@@ -524,7 +421,6 @@ var anticipatedGames = function() {
 
      // makes most ancitipated section clickable
       $('#anticipatedCard' + i).on('click',function(){
-  console.log('pass')
 
     var gameId = data.results[randomNumGen + i].id
     gameIdFunction(gameId)
@@ -539,5 +435,4 @@ var anticipatedGames = function() {
 
 
   var resultBackgroundImage = function (resultBackgroundImage) {
-console.log(resultBackgroundImage)
   }
